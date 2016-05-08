a6ff4d9a6cdc860a421814514725a3dda120ceb6
Muhammad Irfan <Muhammad Irfan>
2016-05-08 19:24:31 +0700

    changed structure to redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,31 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './views/jsx/main.jsx',
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/client.js'
+  ],
   output: {
-    // Output the bundled file.
-    path: './public/javascripts',
-    // Use the name specified in the entry key as name for the bundle file.
-    filename: 'script.js'
+    path: require("path").resolve("./dist"),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  plugins: [  
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
-        // Test for js or jsx files.
-        test: /\.jsx?$/,
+        test: /\.js$/,
+        loader: 'babel-loader',
         exclude: /node_modules/,
-        loader: 'babel'
+        query: {
+          presets: ['react', 'es2015', 'react-hmre']
+        }
       }
     ]
-  },
-  externals: {
-    // Don't bundle the 'react' npm package with the component.
-    'react': 'React' 
-  },
-  resolve: {
-    // Include empty string '' to resolve files by their explicit extension
-    // (e.g. require('./somefile.ext')).
-    // Include '.js', '.jsx' to resolve files by these implicit extensions
-    // (e.g. require('underscore')).
-    extensions: ['', '.js', '.jsx']
-  },
-  //TODO : uncomment on production
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compress: { warnings: false }
-  //   })
-  // ]
-};
+  }
+}
